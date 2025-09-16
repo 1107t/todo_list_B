2af0adca5b4be0890277ea10b40f108f36c954f3
谷藤竜也 <bluehorse091011@yahoo.co.jp>
2025-09-16 12:02:09 +0900

    todo-a_ver4

--- src/components/todos/index.tsx
+++ src/components/todos/index.tsx
@@ -554,7 +554,7 @@ const TodoApp: React.FC = () => {
               paddingLeft: '10px',
               color: '#666'
             }}>
-              > {part.replace('> ', '')}
+              {'>'}  {part.replace('> ', '')}
             </div>
           );
         } else if (part.trim() !== '') {
@@ -633,7 +633,7 @@ const TodoApp: React.FC = () => {
                 <strong>**期限**:</strong> {projectDetail.deadline}
               </div>
               <div style={{ marginBottom: '10px' }}>
-                <strong>*責任者*:</strong> {projectDetail.responsible}
+                <strong>**責任者**:</strong> {projectDetail.responsible}
               </div>
               <div>
                 {renderDescription(projectDetail.description)}
